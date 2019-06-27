bc98cacdec518dbdadf052dc190c2fdcaf66956e
Karen Go <karenrgo@gmail.com>
2019-06-27 15:24:30 -0700

    completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :views, proc { File.join(root, '../views/') }
+  # set :views, 'app/views'
   register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
---
+++ db/migrate/06_landmark_change_integer.rb
@@ -0,0 +1,6 @@
+class LandmarkChangeInteger < ActiveRecord::Migration
+    def change
+      change_column :landmarks, :year_completed, :integer
+    end
+  end
+  
